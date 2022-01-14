ddc516a59c7cdc10a5ee3b0368d6b860af360f47
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-13 23:15:01 -0500

    changed button from input to button

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("form");
+const sum_1_min = document.getElementById("sum_1_min");
+const sum_1_max = document.getElementById("sum_1_max");
+const sum_2_min = document.getElementById("sum_2_min");
+const sum_2_max = document.getElementById("sum_2_max");
+const com_1_min = document.getElementById("com_1_min");
+const com_1_max = document.getElementById("com_1_max");
+const com_2_min = document.getElementById("com_2_min");
+const com_2_max = document.getElementById("com_2_max");
+
+form.addEventListener("submit", function (e) {
+  console.log("submit");
+});
