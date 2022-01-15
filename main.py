3a5562822fc7de5f4d74584db70cecffe2a5cdd5
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-15 18:40:09 -0500

    wrapped com and sum sections in divs

---
+++ main.py
@@ -0,0 +1,5 @@
+a = 1
+b = str(2)
+
+print(a + b)
+print(type(a))
--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const form = document.getElementById("form");
-const sum_1_min = document.getElementById("sum_1_min");
-const sum_1_max = document.getElementById("sum_1_max");
-const sum_2_min = document.getElementById("sum_2_min");
-const sum_2_max = document.getElementById("sum_2_max");
-const com_1_min = document.getElementById("com_1_min");
-const com_1_max = document.getElementById("com_1_max");
-const com_2_min = document.getElementById("com_2_min");
-const com_2_max = document.getElementById("com_2_max");
+const sum_1_min = document.getElementById("sum-1-min");
+const sum_1_max = document.getElementById("sum-1-max");
+const sum_2_min = document.getElementById("sum-2-min");
+const sum_2_max = document.getElementById("sum-2-max");
+const com_1_min = document.getElementById("com-1-min");
+const com_1_max = document.getElementById("com-1-max");
+const com_2_min = document.getElementById("com-2-min");
+const com_2_max = document.getElementById("com-2-max");
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+}
 
 form.addEventListener("submit", function (e) {
-  console.log("submit");
+  e.preventDefault();
+  if (sum_1_min.value === "") {
+    showError(sum_1_min, "input is required");
+  } else {
+    showSuccess(sum_1_min);
+  }
 });
