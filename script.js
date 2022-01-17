d5abf0c1b0d2d45b15d5efbc7345e15359448cb3
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-16 19:11:56 -0500

    addedd a mpre optimal but less readable and buggy solution as a comment. changed field variable in checkRequired function to number for consistancy

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const comInputFields = document
   .querySelectorAll("input");
 
 // Show input error message
+// Makes error messages visible and highlights around the wrong field.
+// Takes in an error message to replace the default in HTML
 function showError(input, message) {
   const formControl = input.parentElement;
   const errorMessage =
@@ -22,9 +24,9 @@ function showError(input, message) {
 // Check Required for input fields in each section
 function checkRequired(inputArray) {
   inputArray.forEach((input) => {
-    input.forEach((field) => {
-      if (field.value === "") {
-        showError(field, "Input is required");
+    input.forEach((number) => {
+      if (number.value === "") {
+        showError(number, "Input is required");
       }
     });
   });
@@ -78,3 +80,44 @@ form.addEventListener("submit", function (e) {
   // in pair is greater than the first one
   checkMaxGTMin([sumInputFields, comInputFields]);
 });
+
+// ============ A MORE OPTIMAL BUT LESS SCALABLE SOLUTION =============
+// The solutions has mistakes in the order of error raising, but instead of looping the input fields 3 times, it happens only once
+//
+// function inputFieldErrorCheck(inputArray, min = 1, max = 99999) {
+//   inputArray.forEach((input) => {
+//     input.forEach((number) => {
+//       // Check Number Range for input fields in each section
+//       // The min and max values are optional but pre-defined at 1, 99999
+//       if (number.value < min || number.value > max) {
+//         showError(
+//           number,
+//           `Only numbers between ${min} and ${max} are accepted`
+//         );
+//       }
+//       // Check Required for input fields in each section
+//       if (number.value === "") {
+//         showError(number, "Input is required");
+//       }
+//     });
+//     // Checks for Max value being greater than Min value for inputs with 2 pair of numbers
+//     if (parseInt(input[0].value) >= parseInt(input[1].value)) {
+//       showError(
+//         input[1],
+//         "Maximum of each number must be greater than it's minimum"
+//       );
+//     }
+//     if (parseInt(input[2].value) >= parseInt(input[3].value)) {
+//       showError(
+//         input[3],
+//         "Maximum of each number must be greater than it's minimum"
+//       );
+//     }
+//   });
+// }
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   inputFieldErrorCheck([sumInputFields, comInputFields], 1, 99999);
+// });
