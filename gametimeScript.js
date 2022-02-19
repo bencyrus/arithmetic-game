93c88089309ec40801b10f056a7bd2ac273f1dd6
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-19 18:51:54 -0500

    created compareAnswer function

--- gametimeScript.js
+++ gametimeScript.js
@@ -1,8 +1,20 @@
-document.addEventListener("DOMContentLoaded", function (e) {
-  // Start Countdown
-  // Show First Question
-});
+console.log(gameSet.questions[1][3]);
 
-document.querySelector(".answer").addEventListener("input", function (e) {
-  console.log("Hi!");
+let answer = document.querySelector(".answer");
+
+// document.addEventListener("DOMContentLoaded", function (e) {
+// Start Countdown
+// Show First Question
+// });
+
+function compareAnswer(input, question) {
+  if (input.value == question[3]) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+answer.addEventListener("input", function (e) {
+  console.log(answer.value);
 });
