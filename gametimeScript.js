e44e765671d0ec97f534661019645c85de74e755
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-19 20:01:34 -0500

    created the timer and score keeping functions

--- gametimeScript.js
+++ gametimeScript.js
@@ -1,6 +1,12 @@
-console.log(gameSet.questions[1][3]);
-
+let questions = gameSet.questions;
+let time = gameSet.duration;
+let tempQuestions = questions;
+let question = gameSet.questions[1];
 let answer = document.querySelector(".answer");
+let score = 0;
+
+let secondsLeft = document.querySelector(".seconds-left");
+let currentScore = document.querySelector(".score-number");
 
 // document.addEventListener("DOMContentLoaded", function (e) {
 // Start Countdown
@@ -8,13 +14,38 @@ let answer = document.querySelector(".answer");
 // });
 
 function compareAnswer(input, question) {
-  if (input.value == question[3]) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return input === question[3];
 }
 
+secondsLeft.innerHTML = time;
+
+setInterval(function () {
+  time--;
+  if (time >= 0) {
+    secondsLeft.innerHTML = time;
+  }
+  if (time === 0) {
+    console.log("Finished");
+  }
+}, 1000);
+
 answer.addEventListener("input", function (e) {
-  console.log(answer.value);
+  if (compareAnswer(parseInt(answer.value), question) === true) {
+    score += 1;
+    currentScore.innerHTML = score;
+  }
 });
+
+// tempQuestions.forEach((element) => {
+//   console.log(element[0], element[1], element[2], element[3]);
+//   answer.addEventListener("input", function (e) {
+//     if (compareAnswer(parseInt(answer.value), question) === true) {
+//       score += 1;
+//     }
+//   });
+//   console.log(score);
+// });
+
+// answer.addEventListener("input", function (e) {
+//     console.log(compareAnswer(parseInt(answer.value), question));
+// });
