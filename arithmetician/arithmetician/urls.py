60cfdb0ab8e24f14e7872b552a6ebb0a54d67011
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-21 23:39:26 -0500

    added url file to arithmetic game

--- arithmetician/arithmetician/urls.py
+++ arithmetician/arithmetician/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('arithmetic-game', include('arithmetic_game.urls'))
 ]
