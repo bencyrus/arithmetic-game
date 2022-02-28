ffc2033870d59211289846650fd7b3a235c8558a
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-28 18:23:35 -0500

    created aview for QusetionsList and set the routes in URL

--- arithmetician/arithmetic_game/urls.py
+++ arithmetician/arithmetic_game/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('game1/', views.game)
+    path('game1/', views.game),
+    path('questions/', views.QuestionsListView.as_view()),
 ]
--- arithmetician/arithmetic_game/views.py
+++ arithmetician/arithmetic_game/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import QuestionsList
+from .serializers import QuestionsListSerializer
 
 def game(request):
     return render(request, 'gamesettings/index.html')
+
+class QuestionsListView(generics.ListCreateAPIView):
+    queryset = QuestionsList.objects.all()
+    serializer_class = QuestionsListSerializer
