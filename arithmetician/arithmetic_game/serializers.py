a2be768539805385035a7e3d7d70482a4bab088b
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-28 18:10:56 -0500

    created QuestionsListSerializer in serializers

--- arithmetician/arithmetic_game/serializers.py
+++ arithmetician/arithmetic_game/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import QuestionsList
+
+class QuestionsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionsList
+        fields = [
+            "addExists",
+            "rangeAddNum12",
+            "rangeAddNum21",
+            "rangeAddNum11",
+            "rangeAddNum22",
+            "mulsExists",
+            "rangeMulNum12",
+            "rangeMulNum21",
+            "rangeMulNum11",
+            "rangeMulNum22"
+            "duration",
+        ]
+        
