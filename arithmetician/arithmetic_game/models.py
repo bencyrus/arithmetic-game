0398e30837b7ddc12f48fec191eeb068919b2b44
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-02-28 17:54:51 -0500

    created QuestionsList model and made migrations

--- arithmetician/arithmetic_game/models.py
+++ arithmetician/arithmetic_game/models.py
@@ -1,5 +1,19 @@
-from ast import Num
-from pyexpat import model
-from turtle import mode
 from django.db import models
 
+class QuestionsList(models.Model):
+    addExists = models.BooleanField()
+    rangeAddNum12 = models.IntegerField()
+    rangeAddNum21 = models.IntegerField()
+    rangeAddNum11 = models.IntegerField()
+    rangeAddNum22 = models.IntegerField()
+
+    mulsExists = models.BooleanField()
+    rangeMulNum12 = models.IntegerField()
+    rangeMulNum21 = models.IntegerField()
+    rangeMulNum11 = models.IntegerField()
+    rangeMulNum22 = models.IntegerField()
+    
+    duration = models.IntegerField()
+
+    # oSign = models.CharField(max_length=1)
+    # answer = models.IntegerField() 
